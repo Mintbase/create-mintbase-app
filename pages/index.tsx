54571995433e2fc71fe8876d1d82b20511d7b70e
Santi García <58360969+SantiTheHuman@users.noreply.github.com>
2022-05-27 21:03:42 +0200

    add project card

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,31 @@ import Hero from '../components/Hero'
 import Container from '../components/Layout/Container'
 import InputForm from '../components/InputForm'
 import {useWallet} from "../services/providers/MintbaseWalletContext";
+import Header from '../components/Header';
+import Card from '../components/Card';
+import birdPic from '../components/bird-hero.webp'
+
 
 const Home = () => {
   const { isConnected } = useWallet()
 
   return (
     <>
+    <Header/>
       <Head>
         <title>Empowering Citizen Scientists</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
         { !isConnected &&
+        <>
       <Container className="flex justify-center my-24" >
          <InputForm></InputForm>
       </Container>
+            <Container className="flex justify-center my-24" >
+            <Card title="Birds of Berlin" team="" description='Help the Natural Science Museum and discover a new side of the city.' deadline="" picture={birdPic}/>
+          </Container>
+     </>
         }
     </>
   )
